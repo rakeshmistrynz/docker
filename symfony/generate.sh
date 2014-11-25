bb6d233e0772252f3b8e62abbc4ac87c07909749
chadrien <chadrien@chadrien.fr>
2014-11-25 22:53:25 +0100

    Fix directories escaping

--- symfony/generate.sh
+++ symfony/generate.sh
@@ -9,7 +9,7 @@ ROOT_DIR=$(dirname $0)
 
 for tag in ${PHP_TAGS[@]}; do
     infos=($(echo ${tag} | tr '-' ' '))
-    to_dir=${ROOT_DIR}/php${infos[0]}/${infos[1]}
+    to_dir="${ROOT_DIR}/php${infos[0]}/${infos[1]}"
     sed_commands="-e 's/{PHP_TAG}/${tag}/'"
 
     if [[ "${infos[0]}" = "5.4" ]]; then
@@ -17,5 +17,5 @@ for tag in ${PHP_TAGS[@]}; do
     fi
 
     mkdir -p ${to_dir}
-    sed "${sed_commands}" ${ROOT_DIR}/Dockerfile-block > ${to_dir}/Dockerfile
+    sed "${sed_commands}" "${ROOT_DIR}/Dockerfile-block" > "${to_dir}/Dockerfile"
 done
