a45b1f3fa5730b25eb2916ecc2973d700aec3add
chadrien <chadrien@chadrien.fr>
2014-11-25 22:50:49 +0100

    No opcache in php5.4

--- symfony/generate.sh
+++ symfony/generate.sh
@@ -10,7 +10,12 @@ ROOT_DIR=$(dirname $0)
 for tag in ${PHP_TAGS[@]}; do
     infos=($(echo ${tag} | tr '-' ' '))
     to_dir=${ROOT_DIR}/php${infos[0]}/${infos[1]}
+    sed_commands="-e 's/{PHP_TAG}/${tag}/'"
+
+    if [[ "${infos[0]}" = "5.4" ]]; then
+        sed_commands="${sed_commands} -e '/opcache/d'"
+    fi
 
     mkdir -p ${to_dir}
-    sed "s/{PHP_TAG}/${tag}/" ${ROOT_DIR}/Dockerfile-block > ${to_dir}/Dockerfile
+    sed "${sed_commands}" ${ROOT_DIR}/Dockerfile-block > ${to_dir}/Dockerfile
 done
